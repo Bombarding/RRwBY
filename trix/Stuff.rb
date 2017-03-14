05d00cb6656613981c95b9a30c044eeb101e7197
Alexander Steel <skruffy@aim.com>
2017-03-13 20:28:32 -0400

    Create Stuff.rb

---
+++ trix/Stuff.rb
@@ -0,0 +1,7 @@
+#https://github.com/jlevy/the-art-of-command-line
+
+#https://github.com/franzejr/best-ruby
+
+#https://github.com/fffaraz/awesome-cpp
+
+#https://github.com/aleksandar-todorovic/awesome-linux
